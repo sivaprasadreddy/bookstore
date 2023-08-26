340c576971e1daca6e532c8e8eccaa423e2e41ec
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-08-26 10:21:43 +0530

    Docker compose run setup

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+declare dc_file="docker-compose.yml"
+
+function build() {
+    ./mvnw clean spotless:apply verify
+}
+
+function build_image() {
+  ./mvnw clean compile spring-boot:build-image -DskipTests -Dspring-boot.build-image.imageName=sivaprasadreddy/bookstore
+}
+
+function start_infra() {
+    docker-compose -f "${dc_file}" up -d
+}
+
+function stop_infra() {
+    docker-compose -f "${dc_file}" stop
+    docker-compose -f "${dc_file}" rm -f
+}
+
+function restart_infra() {
+    stop_infra
+    sleep 5
+    start_infra
+}
+
+function start() {
+    #build_image
+    docker-compose --profile app -f "${dc_file}" up --force-recreate -d
+}
+
+function stop() {
+    docker-compose --profile app -f "${dc_file}" stop
+    docker-compose --profile app -f "${dc_file}" rm -f
+}
+
+function restart() {
+    stop
+    sleep 5
+    start
+}
+
+action="start"
+
+if [[ "$#" != "0"  ]]
+then
+    action=$*
+fi
+
+eval "${action}"
