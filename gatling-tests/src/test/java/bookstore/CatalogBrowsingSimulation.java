527ddba0304ade47236785fb65a209ba6610b110
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-05-24 09:13:23 +0530

    Clean up

--- gatling-tests/src/test/java/bookstore/CatalogBrowsingSimulation.java
+++ gatling-tests/src/test/java/bookstore/CatalogBrowsingSimulation.java
@@ -7,14 +7,27 @@ import static utils.SimulationHelper.getConfig;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.random.RandomGenerator;
+import java.util.stream.Stream;
 import utils.SimulationHelper;
 
 public class CatalogBrowsingSimulation extends Simulation {
 
     HttpProtocolBuilder httpProtocol = SimulationHelper.getHttpProtocolBuilder("app.catalogServiceUrl");
 
-    ChainBuilder browseProducts =
-            exec(http("View Products").get("/api/products")).pause(1);
+    Iterator<Map<String, Object>> randomPageNo = Stream.generate((Supplier<Map<String, Object>>) () -> {
+                int randInt = RandomGenerator.getDefault().nextInt(1, 20);
+                return Collections.singletonMap("pageNo", randInt);
+            })
+            .iterator();
+
+    ChainBuilder browseProducts = feed(randomPageNo)
+            .exec(http("View Products").get("/api/products?page=#{pageNo}"))
+            .pause(1);
 
     ScenarioBuilder scnBrowseProducts = scenario("Browse Products")
             .during(Duration.ofSeconds(60), "Counter")
