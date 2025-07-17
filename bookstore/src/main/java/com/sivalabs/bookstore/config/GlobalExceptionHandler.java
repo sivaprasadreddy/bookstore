7555c60f7736a7240e1fb301876768c1dabddae4
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-07-17 17:44:49 +0530

    feat: Add error pages

--- bookstore/src/main/java/com/sivalabs/bookstore/config/GlobalExceptionHandler.java
+++ bookstore/src/main/java/com/sivalabs/bookstore/config/GlobalExceptionHandler.java
@@ -1,24 +1,18 @@
 package com.sivalabs.bookstore.config;
 
-import java.net.URI;
-import java.time.Instant;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
-    ProblemDetail handleGenericException(Exception e) {
-        ProblemDetail problemDetail =
-                ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        problemDetail.setTitle("Internal Server Error");
-        problemDetail.setType(URI.create("https://api.bookstore.com/errors/server-error"));
-        problemDetail.setProperty("errorCategory", "Generic");
-        problemDetail.setProperty("timestamp", Instant.now());
-        return problemDetail;
+    String handleGenericException(Exception e) {
+        log.error("Unhandled exception", e);
+        return "error/500";
     }
 }
